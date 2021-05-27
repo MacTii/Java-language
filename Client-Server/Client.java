c8f333c8c998278a04c36f1b9fc832d17385340a
Mateusz Kapka <82517103+MacTii@users.noreply.github.com>
2021-05-27 17:03:50 +0200

    Update Client.java

--- Client-Server/Client.java
+++ Client-Server/Client.java
@@ -63,32 +63,34 @@ class ClientEcho
 			
 			out.println(text);
 			out.println(time);
-			out.flush();
+			//out.flush();
 		}
 		scan.close();
 	}
 	
-	void connectWithServer()
+	void connectWithServer() 
 	{
 		try{
 			Socket s = new Socket(host, port);
-			
 			this.initBuffers(s);
 
-			System.out.println("Wpisz 'exit' aby wyłączyć klienta");
+			System.out.println("Wpisz 'exit' aby wylaczyc klienta");
 			System.out.println("Podaj tekst i godzine: ");
 			
 			ClientPrinter clientThread = new ClientPrinter(s);
 			new Thread(clientThread).start();
 			this.communicateWithServer(out, in);
+			s.close();
+			clientThread.stop();
+			
 		}
 		catch(UnknownHostException e1)
 		{
-			System.out.println("Nieznany host - błąd");
+			System.out.println("Nieznany host - blad");
 		} 
 		catch (IOException e2) 
 		{
-			System.out.println("IO - błąd");
+			System.out.println("IO - blad");
 		}
 		catch (ParseException e3)
 		{
@@ -98,35 +100,71 @@ class ClientEcho
 		{
 			System.out.println(e4.getMessage());
 		}
+		finally
+		{
+			try {
+				in.close();
+				out.close();
+				
+			}
+			catch(IOException e)
+			{
+				System.out.println("Blad IO po stronie klienta/ przy zamykaniu");
+			}
+		}
 	}
 }
 
 class ClientPrinter implements Runnable
 {
 	private Socket socket;
-	BufferedReader in;
+	private BufferedReader in;
+	private boolean exit;
 	
 	ClientPrinter(Socket s) throws IOException
 	{
 		socket = s;
 		InputStreamReader ins = new InputStreamReader(s.getInputStream());
 		in = new BufferedReader(ins);
+		exit = false;
 	}
 	
 	public void run()
 	{
 		try
 		{
-			while(true)
+			while(!exit)
 			{
 				if(in.ready())
-					System.out.println("Odpowiedz od serwera: " + in.readLine() + " " + in.readLine());
+					System.out.println("Odpowiedz od serwera: " + in.readLine());
 			}
 		}
 		catch(IOException e)
 		{
 			System.out.println("Błąd");
 		}
+		finally
+		{
+			try
+			{
+				socket.close();
+				in.close();
+			}
+			catch(IOException e)
+			{
+				System.out.println("Blad IO");
+			}
+		}
+	}
+	
+	public void stop()
+    {
+        exit = true;
+    }
+	
+	boolean isThreadRunning()
+	{
+		return exit;
 	}
 }
 
@@ -138,6 +176,6 @@ public class Client
 		int port = 4900;
 		ClientEcho var = new ClientEcho(host,port);
 		var.connectWithServer();
-		System.out.println("Wyłączanie klienta");
+		System.out.println("Wylaczanie klienta");
 	}
 }
